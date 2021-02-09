cda177f1dd5174c1d6fc324a42223b35093f029a
Mira Manger <mira.manger@haufe-lexware.com>
2021-02-09 13:29:24 +0100

    Projektdateien hinzufügen.

---
+++ CsvViewer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsvViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
